96983c7b2aaf90d952ee6ce0686b65e337bb9110
Eugênio Marquis de Oliveira <geninoliveira@gmail.com>
2018-10-08 23:54:00 -0300

    Preparação inicial para estilização do site.
    Instalação e configuração do framework Tailwind, com instalação do plugin do Laravel Mix.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,9 @@
 const mix = require('laravel-mix');
 
+
+/* Work with Tailwind.js */
+require('laravel-mix-tailwind');
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,4 +16,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .tailwind();
